59b8b8087793f882986a755286efddfd3e5fa53b
IthDithWite <davit.shalikiani.1@btu.edu.ge>
2022-10-12 21:51:37 +0400

    initial commit

--- src/main/kotlin/Fractions.kt
+++ src/main/kotlin/Fractions.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val f1 = Fraction()
+    var f1 = Fraction()
 
-    val f2 = Fraction()
+    var f2 = Fraction()
 
     println(f1.toString())
     println(f1)
@@ -13,13 +13,15 @@ fun main() {
 
 interface FractionMaths {
         fun simplifyFraction()
+        fun findGCD(): Int
+        fun findLCM(): Int
         fun addFractions()
         fun subtractFractions()
         fun multiplyFractions()
         fun divideFractions()
 }
 
-open class Fraction (private var numerator: Int, private var denominator: Int): FractionMaths {
+open class Fraction(private var numerator: Int, private var denominator: Int): FractionMaths {
 
     override fun toString(): String {
         return "$numerator / $denominator"
@@ -35,9 +37,55 @@ open class Fraction (private var numerator: Int, private var denominator: Int): 
     }
 
     override fun simplifyFraction() {
-        var i=0: Int
-        while (numerator%2==0 && denominator %2==0) {
+        var i = 0
+        if (numerator % 3 == 0 && denominator % 2 == 0) {
+            while (numerator % 2 == 0 && denominator % 2 == 0) {
+                numerator /= 2
+                denominator /= 2
+
+            }
+        }
+        while (numerator % 2 == 0 && denominator % 2 == 0) {
+            numerator /= 2
+            denominator /= 2
+        }
+    }
+
+    override fun addFractions(other: Any?) { //წილადების დამატება
+        var equalizeDenominators = 0
+
+    }
+
+    override fun divideFractions() { //წილადების გაყოფა
+        TODO("Not yet implemented")
+    }
+
+    override fun multiplyFractions() { // წილადების გამრავლება
+        TODO("Not yet implemented")
+    }
+
+    override fun subtractFractions() { // წილადების გამოკლება
+        TODO("Not yet implemented")
+    }
+
+    override fun findLCM(other: Any?): Int { // უმცირესი საერთო ჯერადი
+        if (other is Fraction) {
+            val gcd = findGCD(other)
+            val lcm = denominator * other.denominator
+        }
+        return 0
+    }
+    override fun findGCD(other: Any?): Int { // უდიდესი საერთო გამყოფი
+        var gcd = 1
+        var i = 1
+        if (other is Fraction) {
+            while (i <= denominator && i <= other.denominator) {
+                if (denominator % i == 0 && other.denominator % i == 0)
+                    gcd = i
+                ++i
+            }
 
         }
+        return gcd
     }
 }
