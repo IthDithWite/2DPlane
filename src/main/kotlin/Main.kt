a292a85ff7aee6462dbfba9d62650674063f58af
IthDithWite <davit.shalikiani.1@btu.edu.ge>
2022-10-12 21:01:02 +0400

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,24 @@
 fun main() {
-    val point1 = Position(5,7)
+    val point1 = Position(5.2,7.3)
     println(point1.toString())
-    val point2 = Position(8,9)
+    val point2 = Position(8.1,9.4)
     print(point1.calcDistance(point2))
 }
 
 interface TwoDPlane {
-    fun moveXSymmetrically()
-    fun moveYSymmetrically()
-    fun calcDistance(other: Any): Double {
-        return 0.2
-    }
+    fun moveXSymmetrically() // x სათავის მიმართ სიმეტრიულად გადატანა
+    fun moveYSymmetrically() // y სათავის მიმართ სიმეტრიულად გადატანა
+    fun calcDistance(other: Any): Double // ორი წერტილის შორის მანძილის გამოთვლა
 
 }
 
 open class Position(private var x: Double, private var y: Double): TwoDPlane {
 
-    override fun toString(): String {
+    override fun toString(): String { // კოორდინატების დაბრუნება
         return "X: $x Y: $y"
     }
 
-    override fun equals(other: Any?): Boolean {
+    override fun equals(other: Any?): Boolean { // შედარება
         if (other is Position) {
             if (x == other.x && y == other.y) {
                 return true
@@ -39,7 +37,7 @@ open class Position(private var x: Double, private var y: Double): TwoDPlane {
 
     override fun calcDistance(other: Any): Double {
         if (other is Position) {
-            return Math.sqrt(Math.pow(x - other.x) + Math.pow(y - other.y)
+            return Math.sqrt(Math.pow(x - other.x) + Math.pow(y - other.y))
         }
         return 0.1
     }
